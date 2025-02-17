10589469e15624c8d0dca6e66816f46e4486546f
PhantomFromNewFolsom <podzizeyal@gmail.com>
2025-02-17 13:06:42 +0500

    Решение задачи "Поиск аптеки 2.0"

--- poisk.py
+++ poisk.py
@@ -1,14 +1,13 @@
-import sys
-from io import BytesIO  # Этот класс поможет нам сделать картинку из потока байт
+from io import BytesIO
+from math import sqrt
 
+import sys
 import requests
 from PIL import Image
-from params import map_params
 
+search_api_server = "https://search-maps.yandex.ru/v1/"
+api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-# Пусть наше приложение предполагает запуск:
-# python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
 toponym_to_find = " ".join(sys.argv[1:])
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
@@ -29,10 +28,62 @@ json_response = response.json()
 # Получаем первый топоним из ответа геокодера.
 toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
 
-map_api_server = "https://static-maps.yandex.ru/v1"
+address_ll = ','.join(toponym["Point"]["pos"].split(' '))
 
+search_params = {
+    "apikey": api_key,
+    "text": "аптека",
+    "lang": "ru_RU",
+    "ll": address_ll,
+    "type": "biz"
+}
+
+response = requests.get(search_api_server, params=search_params)
+if not response:
+    pass
+
+# Преобразуем ответ в json-объект
+json_response = response.json()
+
+# Получаем первую найденную организацию.
+organization = json_response["features"][0]
+# Название организации.
+org_name = organization["properties"]["CompanyMetaData"]["name"]
+# Адрес организации.
+org_address = organization["properties"]["CompanyMetaData"]["address"]
+
+# Получаем координаты ответа.
+point = organization["geometry"]["coordinates"]
+org_point = f"{point[0]},{point[1]}"
+apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+
+
+def map_params(toponym):
+    # Координаты центра топонима:
+    # Долгота и широта:
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
+
+    # Собираем параметры для запроса к StaticMapsAPI:
+    map_params = {
+        "apikey": apikey,
+        "pt": f'{toponym_longitude},{toponym_lattitude},pm2ntl~{org_point},pm2dgl'
+
+    }
+    return map_params
+
+
+toponym_coodrinates = toponym["Point"]["pos"]
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+map_api_server = "https://static-maps.yandex.ru/v1"
 # ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params(toponym))
+print(point[0], float(toponym_longitude), point[1], float(toponym_lattitude))
+way = sqrt((point[0] - float(toponym_longitude)) ** 2 + (point[1] - float(toponym_lattitude)) ** 2)
+print(f'''Расстояние: {way} градусов
+Название: {organization['properties']['CompanyMetaData']['name']}
+Адрес аптеки: {organization['properties']['CompanyMetaData']['address']}
+Время работы: {organization['properties']['CompanyMetaData']['Hours']['text']}''')
 im = BytesIO(response.content)
 opened_image = Image.open(im)
-opened_image.show()  # Создадим картинку и тут же ее покажем встроенным просмотрщиком операционной системы
+opened_image.show()
